75538297dc357724d907292652e849dc61da2de3
drrnmcdnld <drrnmcdnld@gmail.com>
2015-02-11 19:50:59 -0400

    Added the bodyParser and postgres library to the file.

--- index.js
+++ index.js
@@ -1,25 +1,32 @@
-var express = require(‘express’);
+var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+
+app.use(bodyParser.json({ type: 'application/json' }));
+
+var postgres = require('./lib/postgres');
+
 
 // Create the express router object for Photos
 var photoRouter = express.Router();
 
 // A GET to the root of a resource returns a list of that resource
-photoRouter.get(‘/’, function(req, res) { });
+photoRouter.get('/', function(req, res) { });
 
 // A POST to the root of a resource should create a new object
-photoRouter.post(‘/’, function(req, res) { });
+photoRouter.post('/', function(req, res) { });
 
 // We specify a param in our path for the GET of a specific object
-photoRouter.get(‘/:id’, function(req, res) { });
+photoRouter.get('/:id', function(req, res) { });
 
 // Similar to the GET on an object, to update it we can PATCH
-photoRouter.patch(‘/:id’, function(req, res) { });
+photoRouter.patch('/:id', function(req, res) { });
 
 // Delete a specific object
 photoRouter.delete('/:id', function(req, res) { });
 
 // Attach the routers for their respective paths
-app.use(‘/photo’, photoRouter);
+app.use('/photo', photoRouter);
 
 module.exports = app;
