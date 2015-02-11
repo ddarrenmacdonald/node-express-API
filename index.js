ebb3882b9178d89c00958e5ac099ace7adc37f11
drrnmcdnld <drrnmcdnld@gmail.com>
2015-02-11 19:28:31 -0400

    Created an Express Router to the resources.

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-var express = require('express');
+var express = require(‘express’);
 var app = express();
 
-app.get('/', function(req, res) {
-	res.end();
-});
+// Create the express router object for Photos
+var photoRouter = express.Router();
+
+// A GET to the root of a resource returns a list of that resource
+photoRouter.get(‘/’, function(req, res) { });
+
+// A POST to the root of a resource should create a new object
+photoRouter.post(‘/’, function(req, res) { });
+
+// We specify a param in our path for the GET of a specific object
+photoRouter.get(‘/:id’, function(req, res) { });
+
+// Similar to the GET on an object, to update it we can PATCH
+photoRouter.patch(‘/:id’, function(req, res) { });
+
+// Delete a specific object
+photoRouter.delete('/:id', function(req, res) { });
+
+// Attach the routers for their respective paths
+app.use(‘/photo’, photoRouter);
 
 module.exports = app;
