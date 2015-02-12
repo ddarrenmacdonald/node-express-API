c4488b3aa5d60ca480a9f09c835040fabae141a8
drrnmcdnld <drrnmcdnld@gmail.com>
2015-02-12 12:20:14 -0400

    Restarted the tutorial from scratch after reviewing Building Blocks with Express

--- index.js
+++ index.js
@@ -1,78 +1,28 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var expressValidator = require(‘express-validator’);
-
 var app = express();
 
-app.use(bodyParser.json({ type: 'application/json' }));
-// We add the middleware after we load the body parser
-app.use(expressValidator());
-
-var postgres = require('./lib/postgres');
-
-function lookupPhoto(req, res, next) {
-// We access the ID param on the request object
-  var photoId = req.params.id;
-
-
-// Create the express router object for Photos
+//Declared a new express router for Photo path
 var photoRouter = express.Router();
-photoRouter.get('/', function(req, res) { });
-photoRouter.post('/', function(req, res) {
 
-// Build an SQL query to select the resource object by ID
-	var sql = 'Insert into photo (description, filepath, album_id) VALEs ($1, $2, $3) RETURNING id';
-	var data = [
-    req.body.description,
-    req.body.filepath,
-    req.body.album_id
-  ];
+//Used the "chaining" feature to refactor code
+photoRouter.get(‘/’, function(req, res) { });
+photoRouter.post(‘/’, function(req, res) { });
+photoRouter.get(‘/:id’, function(req, res) { });
+photoRouter.patch(‘/:id’, function(req, res) { });
+photoRouter.delete(‘/:id’, function(req, res) { });
+app.use(‘/photo’, photoRouter);
 
-	postgres.client.query(sql, data, function(err, result) {
-	    if (err) {
-	      console.error(err);
-	      res.statusCode = 500;
-	      return res.json({
-	        errors: ['Failed to create photo']
-	      });
-	    }
+//Declared a new express router for Album path
+var albumRouter = express.Router();
 
-	var newPhotoId = result.rows[0].id;
-    var sql = 'SELECT * FROM photo WHERE id = $1';
-    postgres.client.query(sql, [ newPhotoId ], function(err, result) {
-      if (err) {
-        // We shield our clients from internal errors, but log them
-        console.error(err);
-        res.statusCode = 500;
-        return res.json({
-          errors: ['Could not retrieve photo after create']
-        });
-      }
-      // The request created a new resource object
-      res.statusCode = 201;
-      // The result of CREATE should be the same as GET
-      res.json(result.rows[0]);
-  });
-});
+//Used the "chaining" feature to refactor code
+photoRouter.get(‘/’, function(req, res) { });
+albumRouter.get(‘/’, function(req, res) { });
+albumRouter.post(‘/’, function(req, res) { });
+albumRouter.get(‘/:id’, function(req, res) { });
+albumRouter.patch(‘/:id’, function(req, res) { });
+albumRouter.delete(‘/:id’, function(req, res) { });
+app.use(‘/album’, albumRouter);
 
-// A GET to the root of a resource returns a list of that resource
-photoRouter.get('/', function(req, res) { });
-
-// A POST to the root of a resource should create a new object
-photoRouter.post('/', function(req, res) { });
-
-// We specify a param in our path for the GET of a specific object
-photoRouter.get('/:id', function(req, res) {
-	res.json(req.photo);
-});
-
-// Similar to the GET on an object, to update it we can PATCH
-photoRouter.patch('/:id', function(req, res) { });
-
-// Delete a specific object
-photoRouter.delete('/:id', function(req, res) { });
-
-// Attach the routers for their respective paths
-app.use('/photo', photoRouter);
-
+//Exported Node Module
 module.exports = app;
